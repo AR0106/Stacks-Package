4002b8409efb78b91e8e516bce5b0327eae926b6
Alex Ritt <alexritt06@outlook.com>
2023-04-26 20:58:45 -0500

    Initial Commit

---
+++ Stacks.js
@@ -0,0 +1,32 @@
+import style from "./styles/Stacks.module.css"
+
+export function HorizontalStack({children}) {
+    return (
+        <div className={style.HorizontalStack}>
+            {children}
+        </div>
+    );
+}
+
+export function VerticalStack({children}){
+    return (
+        <div className={style.VerticalStack}>
+            {children}
+        </div>
+    );
+}
+
+export const ScrollType = {
+    Horizontal: style.horizontalScroll,
+    Vertical: style.verticalScroll
+}
+
+export function ScrollStack({children, scroll}) {
+    console.log(scroll)
+
+    return (
+        <div className={(scroll == ScrollType.Horizontal) ? ScrollType.Horizontal : ScrollType.Vertical}>
+            {children}
+        </div>
+    );
+}
